f0ae0459bc2cb20fd7b22a482a3fdf6ba0ca8ac6
Emmanuel Nelson <mnelson@onenorth.com>
2014-05-13 19:57:52 -0500

    Error Pages

--- routes/middleware.js
+++ routes/middleware.js
@@ -62,7 +62,26 @@ exports.flashMessages = function(req, res, next) {
 };
 
 
+exports.initErrorHandlers = function(req, res, next) {
 
+    res.err = function(err, title, message) {
+        res.status(500).render('errors/500', {
+            err: err,
+            errorTitle: title,
+            errorMsg: message
+        });
+    }
+
+    res.notfound = function(title, message) {
+        res.status(404).render('errors/404', {
+            errorTitle: title,
+            errorMsg: message
+        });
+    }
+
+    next();
+
+};
 /**
 	Prevents people from accessing protected pages when they're not signed in
  */
