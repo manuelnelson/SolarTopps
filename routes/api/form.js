eb30c56a267b2a2e9d8d748818764957007e1d97
Emmanuel Nelson <mnelson@onenorth.com>
2014-07-26 11:10:04 -0500

    Updated File Upload

--- routes/api/form.js
+++ routes/api/form.js
@@ -7,38 +7,39 @@ var async = require('async'),
 
 exports = module.exports = function(req, res) {
     //file uploaded
-    if(req.files) {
-        var fileName = req.files.fileUpload.name;
-        var serverPath = '/uploadedFormFiles/' + fileName;
-        var localPath = keystone.get('form upload path');
-        fs.readFile(req.files.fileUpload.path, function (err, data) {
-            // ...
-            var newPath = localPath + serverPath;
-            fs.writeFile(newPath, data,
-                function (error) {
-                    if (error) {
-                        res.send({
-                            error: 'Unable To Upload File'
-                        });
-                        return;
-                    }
-                    return res.apiResponse({ fileName: fileName});
-                }
-            );
-        });
-    }
 
-    else{
-        //submit the form
-        var FormResponse = keystone.list('FormResponse');
-        var model = new FormResponse.model({
-            form: req.body.form,
-            response: JSON.stringify(req.body.response)
-        })
+        if(req.files) {
+            var fileName = req.files.fileUpload.name;
+            var serverPath = '/uploadedFormFiles/' + fileName;
+            var localPath = keystone.get('form upload path');
+            fs.readFile(req.files.fileUpload.path, function (err, data) {
+                // ...
+                var newPath = localPath + serverPath;
+                fs.writeFile(newPath, data,
+                    function (error) {
+                        if (error) {
+                            res.send({
+                                error: 'Unable To Upload File'
+                            });
+                            return;
+                        }
+                        return res.apiResponse({ fileName: fileName});
+                    }
+                );
+            });
+        }
+        else{
+            //submit the form
+            var FormResponse = keystone.list('FormResponse');
+            var model = new FormResponse.model({
+                form: req.body.form,
+                response: JSON.stringify(req.body.response)
+            })
             model.formMarkdown.md = req.body.formMarkdown;
             model.save(function(err) {
                 if (err) return res.apiResponse({ success: false, err: err });
                 return res.apiResponse({ success: true });
             });
-    }
+        }
+
 }
